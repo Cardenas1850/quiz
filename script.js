c75d5131adc93a24755d1ded801da63b916a1f47
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-07-21 12:06:23 -0400

    completed adding questions

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title:"Commonly used data types do NOT include",
+        options:["strings", "booleans","alerts","numbers"],
+        answer:"alerts"
+    },
+    {
+        title:"A very useful tool for development and debugging that allows the user to print content to the debugger is:",
+        options:["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        title:"Arrays in Javascript can be used to store:",
+        options:["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer:"all of the above"
+    },
+    {
+        title:"The condition of an if/else statement is enclosed within:",
+        options:["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title:"String values must be enclosed within ___ when being assigned to variables",
+        options:["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
+    }
+];
